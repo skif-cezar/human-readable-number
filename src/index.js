cc141fc3e55ecd4ee086c6dc2504a49eb98112a3
skif-cezar <skif.cezar@gmail.com>
2020-03-12 00:25:01 +0300

    fixed solution

--- src/index.js
+++ src/index.js
@@ -5,18 +5,22 @@ var arr20 = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy'
 module.exports = function toReadable (number){
     if(number < 20){
         return arr10[number];
-    }else if(number >= 20 && number < 100){
+    }
+
+    if(number >= 20 && number < 100){
         if(number % 10 == 0){
             return arr20[number / 10];
         } else return (arr20[Math.floor(number / 10)] + ' ' + arr10[number % 10]);
-    }else if(number > 99){
+    }
+
+    if(number > 99){
         if(number % 100 == 0){
             return (arr10[number / 100] + ' ' + 'hundred');
         }else if(number % 10 == 0){
-            return (arr[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' +
+            return (arr10[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' +
             arr20[(Math.floor(number / 10)) % 10]);
         } else if(number % 100 < 20){
-            return (arr[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' +
+            return (arr10[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' +
             arr10[number % 100]);
         }else {
             return (arr10[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' +
